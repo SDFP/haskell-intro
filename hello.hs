79d0bdd42016f7bbba55e873a91ad277d81d34cc
Jesse Williamson <Jesse@thunklife.local>
2019-08-24 10:55:42 -0700

    added slides and more code examples

--- hello.hs
+++ hello.hs
@@ -1,35 +1,43 @@
 module Hello where
 
   hello :: [Char] -> [Char]
-  hello name = "Hello, " ++ name
+  hello n = "Hello, " ++ n
 
-  add3Numbers :: Num a => a -> (a -> (a -> a))
-  add3Numbers x y z = x + y + z
+  add :: Num a => a -> (a -> a)
+  add x y = x + y
 
   data FirstLast =
     FirstLast String String
+
     deriving (Show)
+
+  type FirstLastTuple = (String, String)
 
   data DOB =
     DOB String
+
     deriving (Show)
 
   data Person =
     Person FirstLast DOB
+
     deriving (Show)
 
   data FirstLastRec =
     FirstLastRec { first :: String
                  , last :: String }
+
                  deriving (Show)
 
   data DOBRec =
     DOBRec { date :: String }
+
     deriving (Show)
 
   data PersonRec =
     PersonRec { name :: FirstLastRec
               , birthday :: DOBRec }
+
               deriving (Show)
 
   data Jesse = Jesse
@@ -44,19 +52,24 @@ module Hello where
 
   data Foo = Bar
            | Baz
+
            deriving (Show)
 
   data WhatIsThis = String
                   | Integer
+
                   deriving (Show)
 
   data Perhaps a = Nope
                  | Yessir a
+
                  deriving (Show)
 
   data Listy a = Nil
                | Cons a (Listy a)
+
                  deriving (Eq)
+
   instance Show a => Show (Listy a) where
     show Nil = "[ ]"
     show (Cons a listy) = "[ " ++ show a ++ ", "++ show listy ++ " ]"
@@ -71,6 +84,10 @@ module Hello where
   addListy :: Num a => Listy a -> a
   addListy Nil = 0
   addListy (Cons n listy) = n + addListy listy
+
+  safeHead :: [a] -> Maybe a
+  safeHead [] = Nothing
+  safeHead (x:_) = Just x
 
   main :: IO ()
   main = print $ hello "SDFP"
